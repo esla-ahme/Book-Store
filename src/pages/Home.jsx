a6bdd44a85b60313a2fd197a51829944ba1acdc0
esla-ahme <eslamahmed9861@gmail.com>
2025-04-19 10:31:00 +0200

    cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import books from "../data/books.json"
+import BookCard from '../components/Cards/BookCard'
+import AuthorCard from '../components/Cards/AuthorCard'
 const Home = () => {
     const title = import.meta.env.VITE_APP_TITLE
-  return (
-    <div>{title}</div>
+    console.log(books)
+    return (
+    <div>{title}
+    
+    <p>Welcome to the Home Page!</p>
+    <BookCard title={"The Great Gatsby"} author={"F. Scott Fitzgerald"} stores={[{
+        name: "Amazon",
+        price: 10.99
+    },
+    {
+        name: "Barnes & Noble",
+        price: 12.99
+    }]} />
+    <AuthorCard name={"F. Scott Fitzgerald"} noOfBooks={books.length} />
+    </div>
   )
 }
 
